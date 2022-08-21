13fa26dbc10b4a5bbf428ed658c2b742e82fa36d
alelubos <alelubos@gmail.com>
2022-08-21 21:24:22 +0200

    add Firestore DB to Chat

--- App.js
+++ App.js
@@ -1,26 +1,28 @@
-import React, { useState } from "react";
-import "react-native-gesture-handler";
+import React from 'react';
+import 'react-native-gesture-handler';
 
 // import navigation screens
-import Start from "./components/Start";
-import Chat from "./components/Chat";
+import Start from './components/Start';
+import Chat from './components/Chat';
 
 // import react Navigation
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 const Stack = createStackNavigator();
 
-export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name='Start'
-          component={Start}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen name='Chat' component={Chat} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName='Start'>
+          <Stack.Screen
+            name='Start'
+            component={Start}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen name='Chat' component={Chat} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
 }
--- components/Chat.js
+++ components/Chat.js
@@ -1,25 +1,62 @@
-import React from "react";
-import { KeyboardAvoidingView, Platform, View } from "react-native";
-import { Bubble, GiftedChat } from "react-native-gifted-chat";
+import React from 'react';
+import { KeyboardAvoidingView, Platform, View } from 'react-native';
+import { Bubble, GiftedChat } from 'react-native-gifted-chat';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+// Firebase initializations
+const firebase = require('firebase');
+require('firebase/firestore');
+// require('firebase/auth');
 
 export default class Chat extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      selectedColor: "",
+      bubbleColor: '',
       messages: [],
+      uid: 0,
     };
+
+    // Initialize & Connect with FIRESTORE DB:
+    if (!firebase.apps.length) {
+      firebase.initializeApp({
+        // Firestore Configuration Credentials
+        apiKey: 'AIzaSyCBLjZWSL6qFPHcnlXuaDXwjuDSQA7dDfM',
+        authDomain: 'topchat-app.firebaseapp.com',
+        projectId: 'topchat-app',
+        storageBucket: 'topchat-app.appspot.com',
+        messagingSenderId: '365702031768',
+        appId: '1:365702031768:web:c81f8784b835cb4f77e3ed',
+        experimentalForceLongPolling: true,
+      });
+      // Reference "messages" collection (to query its documents)
+      this.referenceChatMessages = firebase.firestore().collection('messages');
+    }
   }
 
+  // Adds message to Firestore DB
+  addMessage = (message) => {
+    this.referenceChatMessages.add({
+      _id: message._id,
+      test: message.text,
+      createdAt: message.createdAt,
+      user: message.user,
+      uid: this.state.uid,
+    });
+  };
+
   componentDidMount() {
+    // Get & Set User's name as Chat Title
     let name = this.props.route.params.name;
     this.props.navigation.setOptions({ title: name });
+
+    // Set personalized Bubble color
     this.setState({
-      selectedColor: this.props.route.params.color,
-      messages: [
+      bubbleColor: this.props.route.params.color,
+      /* messages: [
         {
           _id: 1,
-          text: "Hello there Developer... Hope you are having a good day! 👍",
+          text: "Hi Developer... Hope you are having a good day! 👍",
           createdAt: new Date(),
           user: {
             _id: 2,
@@ -33,14 +70,63 @@ export default class Chat extends React.Component {
           createdAt: new Date(),
           system: true,
         },
-      ],
+      ], */
     });
+
+    // AUTHENTICATION
+    /* Call Firestore auth() service, subscribe Anonymous user
+       & save returned function to unsubscribe it */
+    this.authUnsubscribe = firebase.auth().onAuthStateChanged(async (user) => {
+      // Subscribe Anonymous User (if none subscribed)
+      if (!user) {
+        await firebase.auth().signInAnonymously();
+      }
+      //update user state with current active user data
+      this.setState({
+        uid: user.uid,
+        loggedInText: 'Hello there',
+      });
+    });
+
+    // FIRESTORE DB
+    // Reference 'messages' Collection
+    this.referenceChatMessages = firebase.firestore().collection('messages');
+    /* Create "messages" observer, pass it callback (to call with updates)
+     and save returned function (used to unsubscribe observer):  */
+    this.unsubscribe = this.referenceChatMessages
+      .orderBy('createdAt', 'desc')
+      .onSnapshot(this.onCollectionUpdate);
   }
 
+  componentWillUnmount() {
+    // stop listening to the collection changes & authentication
+    this.unsubscribe();
+    this.authUnsubscribe();
+  }
+
+  // Updates messages state after DB update
+  onCollectionUpdate = (querySnapShot) => {
+    const messages = [];
+    querySnapShot.forEach((doc) => {
+      let data = doc.data();
+      messages.push({
+        _id: data._id,
+        createdAt: data.createdAt.toDate(),
+        text: data.text || '',
+        user: data.user,
+      });
+    });
+    this.setState({ messages });
+  };
+
+  // Add message to messages state
   onSend(messages = []) {
+    console.log(messages);
     this.setState((previousState) => ({
       messages: GiftedChat.append(previousState.messages, messages),
     }));
+    // Update Firestore with last message
+    this.addMessage(this.state.messages[0]);
   }
 
   renderBubble(props) {
@@ -49,7 +135,7 @@ export default class Chat extends React.Component {
         {...props}
         wrapperStyle={{
           right: {
-            backgroundColor: this.state.selectedColor,
+            backgroundColor: this.state.bubbleColor,
           },
         }}
       />
@@ -62,10 +148,10 @@ export default class Chat extends React.Component {
         <GiftedChat
           renderBubble={this.renderBubble.bind(this)}
           messages={this.state.messages}
-          onSend={(messages) => this.onSend(messages)}
+          onSend={(message) => this.onSend(message)}
           user={{ _id: 1 }}
         />
-        {Platform.OS === "android" ? (
+        {Platform.OS === 'android' ? (
           <KeyboardAvoidingView behavior='height' />
         ) : null}
       </View>
